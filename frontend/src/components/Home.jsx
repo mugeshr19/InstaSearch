cc01b1ee25bf9e5e35c4864afdd9ad7864b588bc
mugeshr19 <mugesh.r1906@gmail.com>
2026-05-19 17:13:24 +0530

    added debounce so that overloading server with api request will be reduced and the server accessing database will be reduced

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useMemo } from "react";
 import axios from "axios";
 
 
@@ -6,32 +6,46 @@ function Home(){
     const [query, setQuery] = useState("");
     const [users, setUsers] = useState([]);
 
-    const handleSearch = async (e) => {
+    function debounce(func,delay){
+        let timer;
+        return function (...args){
+            clearTimeout(timer);
+            timer = setTimeout(()=> {
+                func(...args);
+            }, delay);
+        };
+    }
 
-        const value = e.target.value;
-
-        setQuery(value);
+    const fetchUsers = async (value) => {
 
         if (!value) {
         setUsers([]);
         return;
         }
-
         try {
-
         const res = await axios.get(
             `http://localhost:3000/api/search?q=${value}`
         );
-
         setUsers(res.data);
 
         } catch (err) {
-
         console.log(err);
 
         }
 
     };
+
+    const debouncedSearch = useMemo(
+        ()=> debounce(fetchUsers,300),[]
+    );
+
+    const handleSearch =async(e) =>{
+        const value = e.target.value;
+        setQuery(value);
+        debouncedSearch(value);
+    };
+
+    
     return(
         <div>
             <input type="text" placeholder="Search username..." value={query} onChange={handleSearch} style={{width: "50%", padding: "10px",fontSize: "16px"}} />
